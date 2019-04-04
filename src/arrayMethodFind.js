81d52c4f82e005da4672a02ba79aabf2ee25d0e5
Vadim Ilchenko <v.ilchenko@mate.academy>
2019-04-04 15:08:14 +0300

    implemented task and tests

---
+++ src/arrayMethodFind.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * Implement method Find
+ */
+function applyCustomFind() {
+  [].__proto__.find2 = function() {
+    // write code here
+  };
+}
+
+module.exports = applyCustomFind;
