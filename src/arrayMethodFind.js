dc75f9740e8282f2100d9f38e43d5ccfa77c98b7
Olegus <gulldan1996@gmail.com>
2019-04-10 17:32:39 +0300

    arrayMethodFind

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -4,8 +4,17 @@
  * Implement method Find
  */
 function applyCustomFind() {
-  [].__proto__.find2 = function() {
-    // write code here
+  [].__proto__.find2 = function(callback) {
+    const items = this;
+
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let shouldBeAdded = callback(item, i, items);
+
+      if (shouldBeAdded) {
+        return item;
+      }
+    }
   };
 }
 
