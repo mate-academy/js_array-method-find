6cf8437a607acefb775a7a4de21bd4451fde74e1
gestap <gestap@gmail.com>
2019-04-15 18:11:38 +0300

    Solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,12 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+      if (callback(element, i, this)) {
+        return element;
+      }
+    }
   };
 }
 
