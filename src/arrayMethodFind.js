93f25ec6d371a8862c07b8a9267780e474cf5c78
VladimirTi <tytmes@mail.ru>
2019-04-18 21:20:22 +0300

    Vladimir`s_solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -4,8 +4,12 @@
  * Implement method Find
  */
 function applyCustomFind() {
-  [].__proto__.find2 = function() {
-    // write code here
+  [].__proto__.find2 = function(callback) {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return this[i];
+      }
+    }
   };
 }
 
