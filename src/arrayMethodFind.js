0ce1b72c739679bae712d1c64420a94987521e8f
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-21 09:54:03 +0300

    solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,14 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    console.log(this, typeof this);
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return this[i];
+      }
+    }
+
+    return undefined;
   };
 }
 
