0f885bf06d04d2e6760b478980d0b19e8c8951eb
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-21 14:14:08 +0300

    Solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -1,12 +1,14 @@
 'use strict';
-
 /**
  * Implement method Find
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return this[i];
+      };
+    };
   };
-}
-
+};
 module.exports = applyCustomFind;
