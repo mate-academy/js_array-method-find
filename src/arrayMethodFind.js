b4d8ed950ae5bb1a34f71e8c120e627e107100a4
scandal0us <d.l.lutsak@gmail.com>
2019-06-21 20:11:47 +0300

    Solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -6,6 +6,13 @@
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
     // write code here
+    const length = this.length;
+    for (let i = 0; i < length; i++) {
+      if (callback(this[i], i, this) === true) {
+        const result = this[i];
+        return result;
+      }
+    };
   };
 }
 
