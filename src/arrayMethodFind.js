d6637ecb82be2029f326fcf06d88ce1aeb4ea6d5
DimaKashyrin <kashyrindima@gmail.com>
2019-06-24 03:57:48 +0300

    solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    let resultArr;
+    const arr = this;
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+      const index = i;
+
+      if (callback(item, index, arr)) {
+        resultArr = item;
+        return resultArr;
+      };
+    }
   };
 }
 
