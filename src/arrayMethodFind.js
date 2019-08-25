08743b434a9d5d5c7f1979f28524a2d73c04fb49
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-25 22:30:45 +0300

    Solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    let searchElement;
+
+    for (let i = 0; i < this.length; i += 1) {
+      if (callback(this[i], i, this)) {
+        searchElement = this[i];
+
+        return searchElement;
+      }
+    }
+
+    return searchElement;
   };
 }
 
