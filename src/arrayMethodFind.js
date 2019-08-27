7c78a8d9481ec5b367adafe14412f3e548d2d720
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-27 18:06:46 +0300

    fix

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,17 +5,13 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    let searchElement;
-
     for (let i = 0; i < this.length; i += 1) {
       if (callback(this[i], i, this)) {
-        searchElement = this[i];
+        const searchElement = this[i];
 
         return searchElement;
       }
     }
-
-    return searchElement;
   };
 }
 
