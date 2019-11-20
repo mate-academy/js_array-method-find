b47cecbd631c2cdbd39966a87399c68911209061
boom40 <kickgames675@gmail.com>
2019-11-21 00:15:45 +0200

    solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    const result = [];
+    for (let i = 0; i < this.length; i++){
+      if (callback(this[i], i, this))
+      result.push(this[i])
+      break
+    }
+    return result;
   };
 }
 
