d1c4ce0bfc6e191df92a462b589963d9afd507bd
Andrey Faychak <faychak333@gmail.com>
2020-04-03 16:21:24 +0300

    'solution'

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -6,6 +6,18 @@
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
     // write code here
+
+    if (typeof callback !== 'function') {
+      return this;
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+          return this[i];
+        }
+      }
+
+      return undefined;
+    }
   };
 }
 
