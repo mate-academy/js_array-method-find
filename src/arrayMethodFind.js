5c41301c4fe4b0b67b3df922cbc5c23b0adf1b9f
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-05 22:24:14 +0300

    Solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    const result = [];
+    let index = 0;
+
+    for (const item of this) {
+      if (callback(item, index, this)) {
+        result.push(item);
+      }
+      index++;
+    }
+
+    return result[0];
   };
 }
 
