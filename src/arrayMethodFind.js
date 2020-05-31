f83084816554801fa2cd491059cf3fc260be8ac8
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-05-31 20:37:27 +0300

    solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,15 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        result.push(this[i]);
+      }
+    }
+
+    return result[0];
   };
 }
 
