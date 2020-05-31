9a62f45a9b6d51cddbd411e932654c972009c843
Denys <gryshko_denys@gmail.com>
2020-05-31 21:35:45 +0300

    solution

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,8 +5,17 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return this[i];
+      }
+    }
+
+    return undefined;
   };
 }
+
+// applyCustomFind();
+// console.log([0, 10, 20, 30].find2(a => a === 10));
 
 module.exports = applyCustomFind;
