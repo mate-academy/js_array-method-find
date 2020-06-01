5bba6ec2520a2d16e0e9d2b823e889fd4671165c
tyooma <flying.alien.godzilla@gmail.com>
2020-06-01 15:39:53 +0300

    Array method Find

--- src/arrayMethodFind.js
+++ src/arrayMethodFind.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFind() {
   [].__proto__.find2 = function(callback) {
-    // write code here
+    let foundValue = 0;
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        foundValue = this[i];
+
+        return foundValue;
+      }
+    }
+
+    return undefined;
   };
 }
 
